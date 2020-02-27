ff9928d13bcaaeeffda454496a6e8ff9b95d5262
ICT-Lab <you@example.com>
2020-02-27 16:44:53 +0200

    my program

---
+++ array.py
@@ -0,0 +1,14 @@
+# fruits=["orange","cherry","banana","berry"]
+# print(fruits[2])
+# print(fruits[-4])
+# print(len(fruits))
+# fruits.append("avocado")
+# print(fruits)
+# fruits.remove("cherry")
+# print(fruits)
+# fruits[1]="watermelon"
+# print(fruits)
+numbers=[45,12,20,56]
+numbers.sort(reverse=True)
+numbers.sort()
+print(numbers)
