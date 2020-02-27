94da27ce23ccb9f471fd784d31b22257acc8a87e
ICT-Lab <you@example.com>
2020-02-27 16:29:02 +0200

    my first project

---
+++ set.py
@@ -0,0 +1,9 @@
+fruits={"orange","cherry","banana","berry"}
+# fruits.add("avocado")
+# print(fruits)
+# fruits.update(["watermelon","avocado","pineple"])
+vegetables={"carrots","cabbages"}
+
+fruits.update(vegetables)
+# fruits.clear()
+print(fruits)
